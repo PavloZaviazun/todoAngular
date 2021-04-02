6483286ce2cb7d266dbe540af67884b4e79764ee
Pavlo Zaviazun <help4engineer@gmail.com>
2021-04-02 18:13:47 +0300

    TodoList

--- todoAngular/src/app/services/auth.service.ts
+++ todoAngular/src/app/services/auth.service.ts
@@ -17,6 +17,12 @@ export class AuthService {
       this.mystorage.setItem('token', el.headers.get('Authorization'));
     });
   }
+
+  // loginAttempt(user): void {
+  //   const her = this.httpClient.post('http://localhost:8080/register', user, {observe : 'response'}).subscribe(el => {
+  //     console.log(el);
+  //   });
+  // }
 }
 // , {
 //   headers: {Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiJ9.3tm1LU8zBo3-IxzmiwqsSH2ZI6Gh8Jifa0F8QCWqPxppHzjMwFgwuBgy0sdXF2ogzAx6RsrCeboheR5UnWZGHA'} }
